adaa0e04227c2e120bfa78336394b049555a56ba
WEAHub <hruiz.dev@gmail.com>
2022-08-25 00:06:31 +0200

    Clean

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,18 +6,13 @@ import { AppService } from './app.service';
 import { UserModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { NewsModule } from './news/news.module';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/cryptomon'), 
     UserModule, 
     AuthModule,
-    NewsModule,
-    JwtModule.register({
-      secret: 'secretKey',
-      signOptions: { expiresIn: '1d' },
-    })
+    NewsModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  Nothing(): string {
+    return 'Nothing here :D';
   }
 }
