ad908b6da63145d2e755e2ef6cadfaaace0a26d7
WEAHub <hruiz.dev@gmail.com>
2022-09-14 09:39:18 +0200

    Cleaning

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,15 +1,20 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
-import { jwtConstants } from './../auth.constants';
+import { ConfigService } from '@nestjs/confnpig';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(
+    configService: ConfigService
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: jwtConstants.secret,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+      signOptions: {
+         expiresIn: configService.get<string>('JWT_EXPIRY')
+      },
     });
   }
 
